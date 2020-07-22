a906268e3c3f6b2801b68e279cc91ecf076c5e0a
Nikhil Miranda <unbxd@unbxds-MacBook-Pro.local>
2020-07-22 23:18:26 +0530

    v0.0.4

--- src/main/lib/fetchCredentials.py
+++ src/main/lib/fetchCredentials.py
@@ -1,21 +1,24 @@
-import requests, json
+import requests, json, os
 
+# if os.path.exists('../resources/tempOrgDetails/tempOrgDetails copy.json'):
+with open('../resources/tempOrgDetails/tempOrgDetails_copy.json', '+r') as f:
+    pay=json.load(f)
+    print(pay)
 
-def writeToJSONfile(data, filename = "../resources/tempOrgDetails/tempOrgDetails copy.json"):
-    with open(filename) as f:
-        print(f)
+        
+        # playlist = json.load(f)
+        # f.seek(0)
+        # f.truncate()
+        # json.dump(playlist,f)
 
-writeToJSONfile({'as'})
+    # url = "https://pim.unbxd.io/pim-rails/api/v1/orgList"
 
-# url = "https://pim.unbxd.io/pim-rails/api/v1/orgList"
+    # payload = {}
+    # headers = {
+    #     'Cookie': '_un_sso_uid=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjpudWxsLCJleHBpcnkiOiIyMDIwLTA3LTI5IDE3OjEyOjQwIFVUQyIsImVtYWlsIjoibmlraGlsLm1pcmFuZGErODBAdW5ieGQuY29tIiwicmVnaW9ucyI6e319.1Kalgd19RdLVC3Zi_oz56VBSAtJXkhQkVewWKnqV9QE; JSESSIONID=O01-UztHU8IZQsyC1IT1PHxtYz_ckg4a5uRvacmI',
+    #     'Content-Type': 'application/json'
+    # }
 
-# payload = {}
-# headers = {
-#     'Cookie': '_un_sso_uid=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjpudWxsLCJleHBpcnkiOiIyMDIwLTA3LTI5IDE3OjEyOjQwIFVUQyIsImVtYWlsIjoibmlraGlsLm1pcmFuZGErODBAdW5ieGQuY29tIiwicmVnaW9ucyI6e319.1Kalgd19RdLVC3Zi_oz56VBSAtJXkhQkVewWKnqV9QE; JSESSIONID=O01-UztHU8IZQsyC1IT1PHxtYz_ckg4a5uRvacmI',
-#     'Content-Type': 'application/json'
-# }
+    # response = requests.request("GET", url, headers=headers, data=payload)
 
-# response = requests.request("GET", url, headers=headers, data=payload)
-
-# print(response.text.encode('utf8'))
-
+    # print(response.text.encode('utf8'))
